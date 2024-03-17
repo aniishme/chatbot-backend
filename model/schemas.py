a689465357997f69683704da9977a5e1487004e0
Anish Sharma <sharmaanish023@gmail.com>
2024-03-17 17:44:52 +0545

    Add password hashing

--- model/schemas.py
+++ model/schemas.py
@@ -2,10 +2,9 @@ from pydantic import BaseModel
 
 class UserCreate(BaseModel):
     name: str
-    username: str
     email: str
     password: str
 
 class UserLogin(BaseModel):
-    username: str
+    email: str
     password: str
--- routes/user.py
+++ routes/user.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from database import get_db
+from passlib.context import CryptContext
 
 
 from model.schemas import UserCreate, UserLogin
@@ -8,9 +9,21 @@ from model.models import User
 
 user_router  = APIRouter()
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 @user_router.post("/register/")
 def register(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = User(name=user.name, username=user.username, email=user.email, password=user.password)
+    # Check if the user already exists
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    # Hash the user's password
+    hash_password = pwd_context.hash(user.password)
+
+    # Create a new user instance and save it to the database
+    db_user = User(name=user.name, email=user.email, password=user.password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -18,7 +31,7 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 
 @user_router.post("/login/")
 def login(user: UserLogin, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.username == user.username).first()
+    db_user = db.query(User).filter(User.email == user.email).first()
     if db_user and db_user.password == user.password:
         return {"message": "Login successful"}
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
