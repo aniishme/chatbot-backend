686725860fbbb14bd3b8fe2c3a4e6bf8298586f5
Anish Sharma <sharmaanish023@gmail.com>
2024-03-17 18:04:50 +0545

    Add access token generation on login

--- routes/user.py
+++ routes/user.py
@@ -2,14 +2,31 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from database import get_db
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+from jose import JWTError, jwt
 
 
 from model.schemas import UserCreate, UserLogin
 from model.models import User
 
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1440
+
 user_router  = APIRouter()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 @user_router.post("/register/")
 def register(user: UserCreate, db: Session = Depends(get_db)):
@@ -33,5 +50,9 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 def login(user: UserLogin, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user.email).first()
     if db_user and pwd_context.verify(user.password, db_user.password):
-        return {"message": "Login successful"}
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": user.email}, expires_delta=access_token_expires
+        )
+        return {"access_token": access_token, "token_type": "bearer"}
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
